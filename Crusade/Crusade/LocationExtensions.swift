f297b58a8116172afcb476d3538e1b68b44c900e
Eric <eric@zigabytes.com>
2019-12-21 11:03:06 -0500

    EZ: Setup base project with assets, extensions, and launch screen.

---
+++ Crusade/Crusade/LocationExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  LocationExtensions.swift
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+// MARK: - MKMultiPoint
+
+public extension MKMultiPoint {
+
+    var coordinates: [CLLocationCoordinate2D] {
+        var coords = [CLLocationCoordinate2D](repeating: kCLLocationCoordinate2DInvalid, count: pointCount)
+        getCoordinates(&coords, range: NSRange(location: 0, length: pointCount))
+        return coords
+    }
+
+}
---
+++ Crusade/Crusade/MainController.swift
@@ -0,0 +1,18 @@
+//
+//  MainController.swift
+//  Crusade
+//
+//  Created by Eric Ziegler on 12/21/19.
+//  Copyright © 2019 Zigabytes. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Constants
+
+let MainControllerId = "MainControllerId"
+
+class MainController: BaseViewController {
+
+}
+
